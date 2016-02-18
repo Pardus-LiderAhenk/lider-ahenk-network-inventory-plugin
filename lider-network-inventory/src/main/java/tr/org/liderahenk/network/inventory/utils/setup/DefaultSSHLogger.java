fc686c8b5f505a1de3435d46914a5732f70a988a
emre.akkaya <emre.akkaya@agem.com.tr>
2016-02-18 10:22:43 +0200

    added SSH logger

--- lider-network-inventory/src/main/java/tr/org/liderahenk/network/inventory/utils/setup/DefaultSSHLogger.java
+++ lider-network-inventory/src/main/java/tr/org/liderahenk/network/inventory/utils/setup/DefaultSSHLogger.java
@@ -3,8 +3,16 @@ package tr.org.liderahenk.network.inventory.utils.setup;
 import org.slf4j.LoggerFactory;
 import org.vngx.jsch.util.Logger;
 
+/**
+ * DefaultSSHLogger works as a bridge between a logging framework and SSH
+ * logger, it is used to wrap external logging framework such as slf4j to allow
+ * for SSH logging integration.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
 public class DefaultSSHLogger implements Logger {
-	
+
 	private org.slf4j.Logger logger = LoggerFactory.getLogger(DefaultSSHLogger.class);
 
 	@Override
@@ -20,20 +28,35 @@ public class DefaultSSHLogger implements Logger {
 			logger.info(message);
 		} else if (level == Level.WARN) {
 			logger.warn(message);
-		} else if (level == Level.ERROR) {
+		} else if (level == Level.ERROR || level == Level.FATAL) {
 			logger.error(message);
 		}
-		// TODO
 	}
 
 	@Override
 	public void log(Level level, String message, Object... args) {
-		
+		if (level == Level.DEBUG) {
+			logger.debug(message, args);
+		} else if (level == Level.INFO) {
+			logger.info(message, args);
+		} else if (level == Level.WARN) {
+			logger.warn(message, args);
+		} else if (level == Level.ERROR || level == Level.FATAL) {
+			logger.error(message, args);
+		}
 	}
 
 	@Override
 	public void log(Level level, String message, Throwable exception) {
-		
+		if (level == Level.DEBUG) {
+			logger.debug(message, exception);
+		} else if (level == Level.INFO) {
+			logger.info(message, exception);
+		} else if (level == Level.WARN) {
+			logger.warn(message, exception);
+		} else if (level == Level.ERROR || level == Level.FATAL) {
+			logger.error(message, exception);
+		}
 	}
 
 }
