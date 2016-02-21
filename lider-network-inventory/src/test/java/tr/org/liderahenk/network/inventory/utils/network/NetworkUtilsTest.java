904653ca38a0a94a960e9efd4b89de8cd8acdfdc
emrekgn <emrekaganakkaya@gmail.com>
2016-02-21 13:15:52 +0200

    updated unit test

--- lider-network-inventory/src/test/java/tr/org/liderahenk/network/inventory/utils/network/NetworkUtilsTest.java
+++ lider-network-inventory/src/test/java/tr/org/liderahenk/network/inventory/utils/network/NetworkUtilsTest.java
@@ -12,12 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import junit.framework.TestCase;
-import tr.org.liderahenk.network.inventory.dto.NmapParametersDto;
 import tr.org.liderahenk.network.inventory.utils.StringUtils;
 
 @RunWith(JUnit4.class)
 public class NetworkUtilsTest extends TestCase {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(NetworkUtilsTest.class);
 
 	@Test
@@ -25,30 +24,23 @@ public class NetworkUtilsTest extends TestCase {
 		assertEquals(true, NetworkUtils.isIpValid("192.168.1.106"));
 		assertEquals(true, NetworkUtils.isIpValid("localhost"));
 	}
-	
+
 	@Test
 	public void ipShouldBeReachable() {
 		assertEquals(true, NetworkUtils.isIpReachable("192.168.1.106"));
 		assertEquals(true, NetworkUtils.isIpReachable("192.168.1.40"));
 	}
-	
+
 	@Test
 	public void findAllIpAddresses() throws IOException, InterruptedException {
 		List<String> ipAddresses = NetworkUtils.findIpAddresses();
 		assertNotNull(ipAddresses);
 		logger.info(StringUtils.join(",", ipAddresses));
 	}
-	
+
 	@Test
 	public void networkShouldBeScanned() throws IOException, InterruptedException {
-		NmapParametersDto params = new NmapParametersDto();
-		ArrayList<String> ipList = new ArrayList<String>();
-		ipList.add("192.168.1.40");
-		ipList.add("192.168.1.41");
-		ipList.add("192.168.1.42");
-		params.setIpList(ipList);
-		params.setTimingTemplate("3");
-		ArrayList<Host> hosts = NetworkUtils.scanNetwork(params);
+		ArrayList<Host> hosts = NetworkUtils.scanNetwork("192.168.1.40-42", null, null, null, "3");
 		assertNotNull(hosts);
 		logger.info(StringUtils.join(",", hosts));
 	}
