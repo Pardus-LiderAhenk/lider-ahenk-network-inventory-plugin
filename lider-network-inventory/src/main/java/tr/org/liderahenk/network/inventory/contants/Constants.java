3db93578da865c89939d53e22d6f579022931a2f
emre.akkaya <emre.akkaya@agem.com.tr>
2016-02-26 19:07:04 +0200

    updated thread count

--- lider-network-inventory/src/main/java/tr/org/liderahenk/network/inventory/contants/Constants.java
+++ lider-network-inventory/src/main/java/tr/org/liderahenk/network/inventory/contants/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 		public static final int CONNECTION_PORT = 22;
 		public static final int SESSION_TIMEOUT = 6000;
 		public static final int CHANNEL_TIMEOUT = 6000;
-		public static final int NUM_THREADS = 50;
+		public static final int NUM_THREADS = 10;
 	}
 	
 	public static final class NMAP_CONFIG {
