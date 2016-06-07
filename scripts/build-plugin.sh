c9db432d6cd86bd5d2b6461c1fcd46d1dacbce99
emre.akkaya <emre.akkaya@agem.com.tr>
2016-06-07 17:15:07 +0300

    update build scripts

---
+++ scripts/build-plugin.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+###
+# This script builds the project and generates Lider (lider-network-inventory.jar) & Lider Console (lider-console-network-inventory.jar) distribution files as well as Ahenk package (network-inventory.deb)
+#
+# Generated files can be found under /tmp/lider-ahenk-network-inventory-plugin/
+###
+
+pushd $(dirname $0) > /dev/null
+PRJ_ROOT_PATH=$(dirname $(pwd -P))
+popd > /dev/null
+echo "Project path: $PRJ_ROOT_PATH"
+
+# Build project
+echo "Building lider & lider-console modules..."
+cd "$PRJ_ROOT_PATH"
+mvn clean install -DskipTests
+echo "lider & lider-console modules built successfully."
+
+# Generate Ahenk package
+echo "Generating Ahenk package..."
+cd "$PRJ_ROOT_PATH"/ahenk-network-inventory
+dpkg-buildpackage -b
+echo "Generated Ahenk package"
+
+EXPORT_PATH=/tmp/lider-ahenk-network-inventory-plugin
+echo "Export path: $EXPORT_PATH"
+
+# Copy resulting files
+echo "Copying generated files to $EXPORT_PATH..."
+mkdir -p "$EXPORT_PATH"
+mv -f "$PRJ_ROOT_PATH"/network-inventory*.deb "$EXPORT_PATH"
+mv -f "$PRJ_ROOT_PATH"/network-inventory*.changes "$EXPORT_PATH"
+cp -rf "$PRJ_ROOT_PATH"/lider-network-inventory/target/lider-*.jar "$EXPORT_PATH"
+cp -rf "$PRJ_ROOT_PATH"/lider-console-network-inventory/target/lider-console-*.jar "$EXPORT_PATH"
+echo "Copied generated files."
+
+echo "Built finished successfully!"
+echo "Files can be found under: $EXPORT_PATH"
