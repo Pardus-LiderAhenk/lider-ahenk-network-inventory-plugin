f7813306d79e8643559cc16da1b8d1ff532bf625
mine.dogan <mine.dogan@agem.com.tr>
2016-06-08 18:06:14 +0300

    command revision for installahenk

--- ahenk-network-inventory/network-inventory/installahenk.py
+++ ahenk-network-inventory/network-inventory/installahenk.py
@@ -3,6 +3,7 @@
 # Author:Mine DOGAN <mine.dogan@agem.com.tr>
 
 import paramiko
+import urllib.request
 
 from base.plugin.abstract_plugin import AbstractPlugin
 
@@ -15,43 +16,33 @@ class InstallAhenk(AbstractPlugin):
         self.logger = self.get_logger()
         self.message_code = self.get_message_code()
 
-        # for remote machine that doesn't have ahenk
-        self.host = self.task['host']
+        self.access_method = self.task['accessMethod']
+        self.install_method = self.task['installMethod']
+        self.ipList = self.task['ipList']
         self.username = self.task['username']
-        self.password = self.task['password']
-        self.deb_path = self.task['path']
-        self.deb_name = self.task['name']
 
-        self.deb_remote_path = '/home/' + self.username + '/' + self.deb_name
-        self.command = 'sudo gdebi -n ' + self.deb_remote_path
+        if self.access_method == 'USERNAME_PASSWORD':
+            self.password = self.task['password']
+
+        if self.install_method == 'APT_GET':
+            self.install_command = 'sudo apt-get install -y --force-yes nmap'  # TODO name for ahenk
+
+        elif self.install_method == 'WGET':
+            self.download_url = self.task['downloadUrl']
+
+        self.deb_path = '/tmp/ahenk.deb'
+        self.command = 'gdebi -n {}'.format(self.deb_path)
 
         self.logger.debug('[NETWORK INVENTORY - installahenk command] Initialized')
 
     def handle_task(self):
         try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(self.host, username=self.username, password=self.password)
-            transport = ssh.get_transport()
-            session = transport.open_session()
-            session.set_combine_stderr(True)
-            session.get_pty()
-
-            self.logger.debug('[NETWORK INVENTORY - installahenk command] SSH connection is started.')
-
-            sftp = ssh.open_sftp()
-            sftp.put(self.deb_path, self.deb_remote_path)
-
-            session.exec_command(self.command)
-            stdin = session.makefile('wb', -1)
-            stdout = session.makefile('rb', -1)
-
-            # you have to check if you really need to send password here
-            stdin.write(self.password + '\n')
-            stdin.flush()
-
-            self.logger.debug('[NETWORK INVENTORY - installahenk command] Ahenk is installing.')
-            print(stdout.read())
+            if self.access_method == 'USERNAME_PASSWORD':
+                for i, val in enumerate(self.ipList):
+                    self.use_username_password(val)
+            elif self.access_method == 'PRIVATE_KEY':
+                for i, val in enumerate(self.ipList):
+                    self.use_key(val)
 
             self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
                                          message='User NETWORK INVENTORY task processed successfully')
@@ -62,6 +53,54 @@ class InstallAhenk(AbstractPlugin):
             self.context.create_response(code=self.message_code.TASK_ERROR.value,
                                          message='A problem occured while handling NETWORK INVENTORY task: {0}'.format(
                                              str(e)))
+
+    def use_username_password(self, host):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, username=self.username, password=self.password)
+        transport = ssh.get_transport()
+        session = transport.open_session()
+        session.set_combine_stderr(True)
+        session.get_pty()
+
+        self.logger.debug('[NETWORK INVENTORY - installahenk command] SSH connection has been started.')
+
+        if self.install_method == 'WGET':
+            urllib.request.urlretrieve(self.download_url, self.deb_path)
+            sftp = ssh.open_sftp()
+            sftp.put(self.deb_path, self.deb_path)
+            session.exec_command(self.command)
+
+        elif self.install_method == 'APT_GET':
+            session.exec_command(self.install_command)
+
+        stdout = session.makefile('rb', -1)
+
+        self.logger.debug('[NETWORK INVENTORY - installahenk command] Ahenk has been installed.')
+
+        print(stdout.read())
+
+    def use_key(self, host):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, username=self.username)
+
+        self.logger.debug('[NETWORK INVENTORY - installahenk command] SSH connection has been started.')
+
+        if self.install_method == 'WGET':
+            urllib.request.urlretrieve(self.download_url, self.deb_path)
+            sftp = ssh.open_sftp()
+            sftp.put(self.deb_path, self.deb_path)
+            stdin, stdout, stderr = ssh.exec_command(self.command)
+
+        elif self.install_method == 'APT_GET':
+            stdin, stdout, stderr = ssh.exec_command(self.install_command)
+
+        self.logger.debug('[NETWORK INVENTORY - installahenk command] Ahenk has been installed.')
+
+        print(stdout.read())
+
+        self.logger.debug('[NETWORK INVENTORY - installahenk command] Ahenk has been installed.')
 
 
 def handle_task(task, context):
