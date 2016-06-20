bc49cad55862aee6acb13d4203b34ab4f6811da9
serendipiri <seren.piri@gmail.com>
2016-06-20 17:23:49 +0300

    taskPlugin boolean property was added to the plugin info of all the plugins.

--- lider-network-inventory/src/main/java/tr/org/liderahenk/network/inventory/plugininfo/PluginInfoImpl.java
+++ lider-network-inventory/src/main/java/tr/org/liderahenk/network/inventory/plugininfo/PluginInfoImpl.java
@@ -29,6 +29,8 @@ public class PluginInfoImpl implements IPluginInfo {
 	private Boolean userOriented;
 
 	private Boolean policyPlugin;
+	
+	private Boolean taskPlugin;
 
 	private Boolean xbased;
 
@@ -40,7 +42,7 @@ public class PluginInfoImpl implements IPluginInfo {
 	public String toString() {
 		return "PluginInfoImpl [pluginName=" + pluginName + ", pluginVersion=" + pluginVersion + ", description="
 				+ description + ", machineOriented=" + machineOriented + ", userOriented=" + userOriented
-				+ ", policyPlugin=" + policyPlugin + ", xbased=" + xbased + "]";
+				+ ", policyPlugin=" + policyPlugin + ", taskPlugin=" + taskPlugin + ", xbased=" + xbased + "]";
 	}
 
 	public String prettyPrintConfig() {
@@ -114,4 +116,12 @@ public class PluginInfoImpl implements IPluginInfo {
 		this.xbased = xbased;
 	}
 
+	@Override
+	public Boolean getTaskPlugin() {
+		return taskPlugin;
+	}
+
+	public void setTaskPlugin(Boolean taskPlugin) {
+		this.taskPlugin = taskPlugin;
+	}
 }
