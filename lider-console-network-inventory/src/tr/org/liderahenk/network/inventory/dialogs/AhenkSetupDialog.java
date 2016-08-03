f9ca8374148c75a56ffc1b76f965ea7db83e33de
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-08-03 17:14:06 +0300

    Removed unnecessary imported packages

--- lider-console-network-inventory/src/tr/org/liderahenk/network/inventory/dialogs/AhenkSetupDialog.java
+++ lider-console-network-inventory/src/tr/org/liderahenk/network/inventory/dialogs/AhenkSetupDialog.java
@@ -13,7 +13,6 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
