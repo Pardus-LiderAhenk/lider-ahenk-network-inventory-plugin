29f82eedd8d899e9ce6ffad9598f05d706bdcd42
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-26 16:15:20 +0300

    fix 1.0.0-beta version

--- lider-console-network-inventory/src/tr/org/liderahenk/network/inventory/constants/NetworkInventoryConstants.java
+++ lider-console-network-inventory/src/tr/org/liderahenk/network/inventory/constants/NetworkInventoryConstants.java
@@ -4,7 +4,7 @@ public class NetworkInventoryConstants {
 	
 	public static final String PLUGIN_NAME = "network-inventory";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 	
 	public static final String SCAN_COMMAND = "SCANNETWORK";
 	
